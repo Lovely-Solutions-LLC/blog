819cb32f62785cdff1185c73611a4109f5684332
Liam Bailey <liambailey131@gmail.com>
2025-09-14 14:11:11 -0400

    first commit

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -16,20 +16,18 @@ export function BlogPosts() {
           return 1
         })
         .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
-            </div>
-          </Link>
+<Link key={post.slug} href={`/blog/${post.slug}`} className="block mb-4">
+  <div className="w-full flex flex-col md:flex-row md:items-baseline md:space-x-3">
+    <p className="text-neutral-600 dark:text-neutral-400 tabular-nums whitespace-nowrap md:w-44">
+      {formatDate(post.metadata.publishedAt, false)}
+    </p>
+    <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+      {post.metadata.title}
+    </p>
+  </div>
+</Link>
+
+
         ))}
     </div>
   )
